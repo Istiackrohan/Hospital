391dd24aba474c835a28fa83b44848c44f4c10d4
Istiackrohan <istiackrohan@gmail.com>
2022-07-28 00:22:00 +0600

    khali login tai baki r shb clear Alhamdulillah

--- src/Pages/Booking/Booking.js
+++ src/Pages/Booking/Booking.js
@@ -9,16 +9,40 @@ const Booking = () => {
     const service = ServiceData.find(service => {
         return service.id === serviceid;
     });
-    const { doctors } = service;
-    const { } = doctors;
+    const { doctors, serviceName, image } = service;
+    const doctor = doctors.find(doctor => {
+        return doctor.doctorName === bookingid;
+    });
+    const { img, doctorName, fees, time, experience } = doctor;
     const { user } = useAuth();
 
+    const [booking, setBooking] = useState("Request booking");
+
+    const isRequestSent = () => {
+        setBooking(booking==="Request booking" ? "Cancel booking" : "Request booking");
+        alert(booking==="Request booking" ? "Congratulations! Your booking request has been sent to aur agent.": "Your booking request has been cancelled.");
+    }
+
     return (<>
-        {!user ? <Navigate to="/login" /> : 
-         <div>
-            <h1>This is {bookingid}</h1>
-         </div>
-         }
+        {!user ? <Navigate to="/login" /> :
+            <div className="flex flex-col">
+                <img src={image} className="relative" alt="" />
+                <h1 className="p-5 w-full text-xl text-white font-bold bg-gray-900 flex justify-center">{serviceName}</h1>
+                <div className="">
+                    <div className="lg:basis-1/3 md:basis-1/2 sm:basis-1 mb-5 px-5 flex justify-center">
+                        <div className="p-6 flex rounded-lg shadow-lg bg-white max-w-sm">
+                            <img className="rounded-full w-32 h-full" src={img}/>
+                            <div className="pl-6">
+                                <h5 className="text-gray-900 text-xl font-medium mb-2">{doctorName}</h5>
+                                <p className="text-gray-700 text-base mb-4">{experience}</p>
+                                <p className="text-gray-700 text-base mb-4">Are you sure that you want to book Mr. <b>{doctorName}'s</b> appointment at <b>{time}</b> for fees of <b>{fees}</b>?</p>
+                                <button type="button" onClick={isRequestSent} className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">{booking}</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        }
     </>);
 };
 
--- src/Pages/Service/ServiceDetails.js
+++ src/Pages/Service/ServiceDetails.js
@@ -15,27 +15,25 @@ const ServiceDetails = () => {
             <div className="container bg-gray-100">
                 <div className="mb-5">
                     <img className="relative" src={service?.image} alt="" />
-                    <h1 className="p-5 text-xl text-white font-bold bg-gray-900">{service?.serviceName}</h1>
+                    <h1 className="p-5 w-full flex justify-center text-xl text-white font-bold bg-gray-900">{service?.serviceName}</h1>
                 </div>
                 <div className="flex flex-wrap">
                     {
                         doctors.map((doctor, i) => {
-                            const { image, doctorName, fees, time, experience } = doctor;
+                            const { img, doctorName, experience } = doctor;
                             return (
                                 <div className="lg:basis-1/3 md:basis-1/2 sm:basis-1 mb-5 px-5 flex justify-center" key={i}>
                                     <div className="">
                                         <div className="rounded-lg shadow-lg bg-white max-w-sm">
                                             <div className="p-6">
                                                 <div className="flex justify-center">
-                                                    <img className="rounded-full w-32 h-32" src={image} alt="" />
+                                                    <img className="rounded-full w-32 h-full" src={img} alt="" />
                                                 </div>
-                                                <h5 className="text-gray-900 text-xl font-medium mb-2">{doctorName}</h5>
+                                                <h5 className="mt-5 text-gray-900 text-xl font-medium mb-2">{doctorName}</h5>
                                                 <p className="text-gray-700 text-base mb-4">{experience}</p>
-                                                <p className="text-gray-700 text-base mb-4">{time}</p>
-                                                <p className="text-gray-700 text-base mb-4">{fees}</p>
 
-                                                <Link to={`/service/${serviceid}/${i}`}>
-                                                    <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Request booking</button>
+                                                <Link to={`/service/${serviceid}/${doctorName}`}>
+                                                    <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Booking Details</button>
                                                 </Link>
                                             </div>
                                         </div>
